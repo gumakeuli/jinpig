44a11cbf56a53e1379ab4ac49e7db42818fd324e
김상원 <gumakeuli@gmail.com>
2025-11-28 19:21:07 +0900

    Flip sword rotation 180 degrees (to -90 degrees)

--- js/slash.js
+++ js/slash.js
@@ -61,9 +61,9 @@ class Slash {
         // 찌르기 거리만큼 이동
         ctx.translate(this.currentReach, 0);
 
-        // 검 이미지 회전 (검 끝이 오른쪽을 향하도록 90도 보정)
-        // 이미지가 위쪽(90도)을 향하고 있다고 가정
-        ctx.rotate(Math.PI / 2);
+        // 검 이미지 회전 (검 끝이 오른쪽을 향하도록 -90도 보정)
+        // 현재 반대 방향이라고 하므로 180도 회전
+        ctx.rotate(-Math.PI / 2);
 
         ctx.drawImage(
             this.image,
